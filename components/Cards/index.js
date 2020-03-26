901b0083fdf5ddced4a715027e7bb3aa9f7d7c01
gconnell29 <gconnelljr29@gmail.com>
2020-03-25 21:20:41 -0400

    created cardMaker function for more modular code

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -25,45 +25,47 @@
 
 const cardsCont = document.querySelector('.cards-container');
 
+let cardMaker = (title, image, name) => {
+  // create DOM elements
+  const card = document.createElement('div');
+  const headline = document.createElement('div');
+  const author = document.createElement('div');
+  const imgCont = document.createElement('div');
+  const img = document.createElement('img');
+  const credit = document.createElement('span');
+  
+  // assign classes/attribute
+  card.classList.add('card');
+  headline.classList.add('headline');
+  author.classList.add('author');
+  imgCont.classList.add('img-container');
+  
+  // append structure
+  card.appendChild(headline);
+  card.appendChild(author);
+  author.appendChild(imgCont);
+  imgCont.appendChild(img);
+  card.appendChild(credit);
+  cardsCont.appendChild(card);
+
+  //populate elements
+  headline.innerHTML = title;
+  img.src = image;
+  credit.innerHTML = `By ${name}`;
+}
+
 axios.get("https://lambda-times-backend.herokuapp.com/articles")
 .then(response => {
   const articleList = response.data.articles; // an object of an array of objects {[{4 pairs}], [{4 pairs}], [{4 pairs}], [{4 pairs}]}
   const articleLength = Object.keys(articleList);
-  console.log(articleList);
+  // console.log(articleList);
   
   // loop through 5 objects
   for (let i = 0; i < articleLength.length; i++) {
     const idvLength = articleList[articleLength[i]];
   // loop through array of each object
     for (let j = 0; j < idvLength.length; j++) {
-      // create DOM elements
-      let card = document.createElement('div');
-      let headline = document.createElement('div');
-      let author = document.createElement('div');
-      let imgCont = document.createElement('div');
-      let img = document.createElement('img');
-      let credit = document.createElement('span');
-      
-      // assign classes/attribute
-      card.classList.add('card');
-      headline.classList.add('headline');
-      author.classList.add('author');
-      imgCont.classList.add('img-container');
-      
-      //populate elements
-      headline.innerHTML = idvLength[j]['headline'];
-      // img.setAttribute('src', idvLength[j]['authorPhoto']);
-      img.src = idvLength[j]['authorPhoto'];
-      credit.innerHTML = `By ${idvLength[j]['authorName']}`;
-      
-      // append structure
-      card.appendChild(headline);
-      card.appendChild(author);
-      author.appendChild(imgCont);
-      imgCont.appendChild(img);
-      card.appendChild(credit);
-      cardsCont.appendChild(card);
-      
+      cardMaker(idvLength[j]['headline'], idvLength[j]['authorPhoto'], idvLength[j]['authorName']);
     }; // end for loop j
   }; // end for loop i
 }) // end .then
